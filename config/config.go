2e16b35f0a17fdd0a5183957ebf8377604dc45e0
Will <651833+WillBeebe@users.noreply.github.com>
2024-08-18 01:35:29 -0700

    switch ssl mode

--- config/config.go
+++ config/config.go
@@ -21,26 +21,42 @@ func Load() (*Config, error) {
 		// env file not used in deployed environments, don't error
 		// return nil, fmt.Errorf("error loading .env file: %w", err)
 	}
-	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require",
+
+	postgresPort := os.Getenv("POSTGRES_PORT")
+	if postgresPort == "" {
+		postgresPort = "5432"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	sslMode := "require"
+	if os.Getenv("LOCAL_DB") != "" {
+		sslMode = "disable"
+	}
+
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		os.Getenv("POSTGRES_USER"),
 		"****",
 		os.Getenv("POSTGRES_SERVER"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"))
+		postgresPort,
+		os.Getenv("POSTGRES_DB"),
+		sslMode)
 	fmt.Println(url)
 
 	return &Config{
-		// DatabaseURL: fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		DatabaseURL: fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=require",
+		DatabaseURL: fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 			os.Getenv("POSTGRES_USER"),
 			os.Getenv("POSTGRES_PASSWORD"),
 			os.Getenv("POSTGRES_SERVER"),
-			os.Getenv("POSTGRES_PORT"),
-			os.Getenv("POSTGRES_DB")),
+			postgresPort,
+			os.Getenv("POSTGRES_DB"),
+			sslMode),
 		JWTSecret:       os.Getenv("JWT_SECRET"),
 		GoogleClientIDs: parseGoogleClientIDs(os.Getenv("GOOGLE_CLIENT_IDS")),
-		// todo: add defaults for some of these
-		Port: os.Getenv("PORT"),
+		Port:            port,
 		EmailWhitelist: []string{
 			"radiatus.io",
 			// Add more allowed domains or full email addresses here
